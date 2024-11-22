1f1ee38f34198f78456dcb211f55843f89165138
LewisMagangi <lewismomanyi34@gmail.com>
2024-11-22 14:46:20 +0000

    Sum type anotation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,2 @@
-#!/usr/bin/env python3
-""" 0. Basic annotations - add
-"""
-
-
-def add(a: float, b: float) -> float:
-    """ Adds two float number numbers together.
-    """
-    return a + b
+def add(a: float, b:int) -> float:
+    return sum(a, b)
--- 0x00-python_variable_annotations/3-to_str.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-""" 3. Basic annotations - to string
-"""
-
-
-def to_str(n: float) -> str:
-    """ Returns string representation of float number
-    """
-    return str(n)
