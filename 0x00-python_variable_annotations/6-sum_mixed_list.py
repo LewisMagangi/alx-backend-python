542a5760141c223bc9382d2f68719312b3432416
Liquelaliqour <lewismomanyi34@gmail.com>
2024-11-22 18:58:44 +0300

    Right Order anotation

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     a type-annotated function sum_mixed_list
     which takes a list mxd_lst of integers
