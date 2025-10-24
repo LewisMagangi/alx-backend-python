c3ec8d8f6788b92680cec346f3cc189a362800f3
Liquelaliqour <lewismomanyi34@gmail.com>
2025-10-24 17:42:03 +0300

    Update Django settings to use MySQL in CI and SQLite locally

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -113,12 +113,29 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import os
+
+if os.environ.get('GITHUB_WORKFLOW') or os.environ.get('CI') or os.environ.get('MYSQL_DB'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ.get('MYSQL_DATABASE', 'test_db'),
+            'USER': os.environ.get('MYSQL_USER', 'test_user'),
+            'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'test_pass'),
+            'HOST': os.environ.get('MYSQL_HOST', '127.0.0.1'),
+            'PORT': os.environ.get('MYSQL_PORT', '3306'),
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            },
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
